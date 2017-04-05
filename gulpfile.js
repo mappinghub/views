e3b4819874c940bd2c129d1f1876e7998acb437c
Daniel Taub <dmtaub@gmail.com>
2017-04-05 15:54:30 -0400

    fixing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,48 +5,26 @@ var _ = require('underscore');
 var jsoncombine = require("gulp-jsoncombine");
 
 var addGulpTask = function(repo){
-	return gulp.src('site/build/'+repo+'/**/*.jsonld')
+  return gulp.src('site/build/'+repo+'/**/*.jsonld')
         .pipe(jsoncombine(repo+'.jsonld', function (data) {
             return new Buffer.from(JSON.stringify(_.map(data, function(value,key){
-            	// add github url for entry
-				value.githubURL = "https://github.com/mappinghub/views/tree/master/views/"+repo+'/'+key+'sonld';
-				return value;
-			})));
+              // add github url for entry
+        value.githubURL = "https://github.com/mappinghub/views/tree/master/views/"+repo+'/'+key+'sonld';
+        return value;
+      })));
     }))
-    .pipe(gulp.dest('./dist/build'));
+    .pipe(gulp.dest('./site/build'));
 }
 
 
 
 gulp.task('default', function(){
-	// compile elements
-	addGulpTask("elements");
+    // compile elements
+    addGulpTask("elements");
 
     //compile mappings
     addGulpTask("mappings");
-
-    // gulp.src('views/mappings/**/*.jsonld')
-    // 	.pipe(jsoncombine('mappings.jsonld', function (data) {
-    //         return createBuffer(data);
-    // }))
-    // .pipe(gulp.dest('./dist/build'));
 });
-
-// //    Edit JSON with function
-//
-// gulp.src('jsonFiles/**/*.json')
-//     .pipe(merge({
-//         fileName: 'file.json',
-//         edit: (parsedJson, file) => {
-//             if (parsedJson.someValue) {
-//                 delete parsedJson.otherValue;
-//             }
-
-//             return parsedJson;
-//         },
-//     }))
-//     .pipe(gulp.dest('./dist'));
-
 
 // //   Provide a default object (files are merged in order so object values will be overwritten)
 
