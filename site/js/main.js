12624b119702df848f595db8bde263f3d43dbe82
Daniel Taub <dmtaub@gmail.com>
2017-04-05 17:01:36 -0400

    using modals to show detail, reformat html again

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,39 @@
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
 
 var createLinkCell = function (nTd, sData, oData, iRow, iCol) {
-  $(nTd).html("<a target='_blank' href='"+oData.githubURL+"'>"+oData.name+"</a>");
+  var elt = $("<a data-href='"+oData.githubURL+"'>"+oData.name+"</a>");
+  $(nTd).html(elt);
+  elt.click(function(){
+    $.ajax({
+        type: "GET",
+        url: elt.data("href"),
+        success: function(msg)
+        {
+          $("#getCodeModal .modal-title").text("Detail View: "+oData.name);
+          $("#getCodeModal").modal("toggle");
+          //var str = JSON.stringify(JSON.parse(msg), undefined, 2);
+          $("#getCode").html(syntaxHighlight(msg));
+        }
+    });
+  });
+
 };
 
 $(function(){
